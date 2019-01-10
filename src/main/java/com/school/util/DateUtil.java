914336560a781a601d2363aab6128db0d41d3f02
xitao <westartourfuture@Outlook.com>
2019-01-10 18:19:08 +0800

    2019年1月10日18:19:01 xitao

--- src/main/java/com/school/util/DateUtil.java
+++ src/main/java/com/school/util/DateUtil.java
@@ -13,11 +13,12 @@ public class DateUtil {
 
     /**
      * 获取时间String
+     *
      * @param date
      * @return
      */
-    public static String getDate(Date date){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd hh:mm:ss");
+    public static String getDate(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String h = simpleDateFormat.format(date);
         System.out.println(h);
         return h;
@@ -25,11 +26,12 @@ public class DateUtil {
 
     /**
      * 获取时间date
+     *
      * @param s
      * @return
      */
-    public static Date getDate(String s){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd hh:mm:ss");
+    public static Date getDate(String s) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd ");
         Date date = null;
         try {
             date = simpleDateFormat.parse(s);
@@ -39,6 +41,19 @@ public class DateUtil {
         return date;
     }
 
+    /**
+     * 时间戳转 String
+     * @param args
+     * @return
+     */
+    public static String DateTimeTOString(String args) {
+        String format;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        long lt = new Long(args);
+        Date date = new Date(lt);
+        format = simpleDateFormat.format(date);
+        return format;
+    }
 
 
 }
